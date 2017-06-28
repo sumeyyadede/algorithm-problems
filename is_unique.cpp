68c22e6dc55e31c2b25d0a6fffa44a731f60ba74
Sumeyya <sumeyya@Omers-MBP-3.lan>
2017-06-28 18:24:12 +0200

    add cpp impl. of is_unique

---
+++ is_unique.cpp
@@ -0,0 +1,31 @@
+#include <map>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+
+bool isUniqueHT(string str) {
+
+	map<char, int> ht;
+	char* cstr = (char*) str.c_str();
+
+	for(int i = 0;i < str.length(); i++) {
+		if(ht[cstr[i]] == 0){
+			ht[cstr[i]] = 1;
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, char **argv) {
+
+	string input = "Sumeya";
+
+	cout << isUniqueHT(input) << endl;
+	
+	return 0;
+}				
