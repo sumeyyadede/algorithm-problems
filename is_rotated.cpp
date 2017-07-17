4ee0ab20bdcc0b7fcb1b8bea04bac535227fc536
Sumeyya Dede <dedesumeyya@gmail.com>
2017-07-17 23:22:06 +0200

    added is_rotated.cpp

---
+++ is_rotated.cpp
@@ -0,0 +1,11 @@
+
+using namespace std;
+bool isSubstr(std::string s, std::string sub) {
+    return s.find(sub) != std::string::npos;
+}
+
+bool isRotated() {
+	string1 = string1.append(string1);
+	return isSubstr(string1, string2);
+}
+
