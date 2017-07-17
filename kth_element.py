a729bb81b60b7c8ec3f007bc9a3a9e4f21cd75c6
Sumeyya Dede <dedesumeyya@gmail.com>
2017-07-17 23:49:52 +0200

    mass commit for the impl. of homeworks

--- kth_element.py
+++ kth_element.py
@@ -4,11 +4,21 @@ def kth_element(linked_list,counter):
 	node = linked_list.head
 	prev_node = linked_list.head
 
-	for i in xrange(counter-1):
-		if node.next != None:
-			node = node.next
-		else:
-			return "could not found this last number."
+	i=0
+	while node:
+		if i==counter-1:
+			break
+		node = node.next
+		i = i+1
+	if i !=  counter-1:
+		print("not enough elements in the list")
+		return #None
+
+	#for i in xrange(counter-1):
+	#	if node.next != None:
+	#		node = node.next
+	#	else:
+	#		return "could not found this last number."
 
 	while node.next != None:
 		node = node.next
@@ -27,11 +37,11 @@ def main():
 	ll.insert(90)
 	ll.insert(80)
 	ll.insert(70)
-	#ll.insert(60)
-	#ll.insert(50)
-	#ll.insert(40)
-	#ll.insert(30)
-	#ll.insert(20)
+	ll.insert(60)
+	ll.insert(50)
+	ll.insert(40)
+	ll.insert(30)
+	ll.insert(20)
 	#ll.insert(10)
 	print_ll(ll.head)
 	print("return the 5th to last element.")
