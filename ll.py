2e65fcfa0b0b40334c2de1cf2101cfbac3d1390f
Sumeyya Dede <dedesumeyya@gmail.com>
2017-07-17 23:20:11 +0200

    added ll.py

---
+++ ll.py
@@ -0,0 +1,54 @@
+
+class Node(object):
+	def __init__(self, data, node = None):
+		self.data = data
+		self.next = node
+
+class LinkedList(object):
+	def __init__(self):
+		self.head = None
+
+	def insert(self, data):
+		n = Node(data)
+		n.next = self.head
+		self.head = n
+
+	def pop(self):
+		t = None
+		if self.head:
+			t = self.head.data
+			self.head = self.head.next
+		return t
+
+	def is_empty(self):
+		return self.head == None
+
+
+def main():
+	ll = LinkedList()
+	ll.insert(1)
+	ll.insert(2)
+	ll.insert(10)
+	ll.insert(20)
+	ll.insert(30)
+	ll.insert(40)
+	ll.insert(50)
+	ll.insert(60)
+	ll.insert(70)
+	ll.insert(80)
+	ll.insert(90)
+	sr = ll.head
+	fr = ll.head
+	while sr:
+		print("sr", sr.data)
+		sr = sr.next
+		if fr:
+			print("fr", fr.data)
+			if fr.next:
+				fr = fr.next.next
+			else:
+				fr = None
+		print("----")
+
+if __name__ == "__main__":
+	main()
